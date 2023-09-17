973f1fd69b92b85075eba249c9c26d07a7b3dd58
Ly Quang Thai <thai.ly@masterisehomes.com>
2023-09-17 16:47:03 +0700

    fix change cluster for pusher

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -5,17 +5,14 @@ export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
-  cluster: 'eu',
+  cluster: "ap1",
   useTLS: true,
 });
 
-export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  {
-    channelAuthorization: {
-      endpoint: '/api/pusher/auth',
-      transport: 'ajax',
-    },
-    cluster: 'eu',
-  }
-);
+export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+  channelAuthorization: {
+    endpoint: "/api/pusher/auth",
+    transport: "ajax",
+  },
+  cluster: "ap1",
+});
